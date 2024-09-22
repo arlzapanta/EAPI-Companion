1aa8a82cad8dac685428a86eee5526cbe21ddde1
akainzapantadev <arl.zapanta@euroasiapharmainc.com>
2024-09-23 07:41:42 +0800

    Updated get location util

--- src/utils/currentLocation.ts
+++ src/utils/currentLocation.ts
@@ -1,3 +1,31 @@
+// // utils/locationUtil.ts
+// import * as Location from 'expo-location';
+
+// interface LocationData {
+//   latitude: number;
+//   longitude: number;
+// }
+
+// export const getLocation = async (): Promise<LocationData | null> => {
+//   try {
+//     // Request location permissions
+//     const { status } = await Location.requestForegroundPermissionsAsync();
+//     if (status !== 'granted') {
+//       throw new Error('Permission to access location was denied');
+//     }
+
+//     // Get current location
+//     const { coords } = await Location.getCurrentPositionAsync();
+//     return {
+//       latitude: coords.latitude,
+//       longitude: coords.longitude,
+//     };
+//   } catch (error) {
+//     console.error('Error getting location:', error);
+//     return null;
+//   }
+// };
+
 // utils/locationUtil.ts
 import * as Location from 'expo-location';
 
@@ -14,8 +42,11 @@ export const getLocation = async (): Promise<LocationData | null> => {
       throw new Error('Permission to access location was denied');
     }
 
-    // Get current location
-    const { coords } = await Location.getCurrentPositionAsync();
+    const { coords } = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High, 
+      timeInterval: 10000
+    });
+
     return {
       latitude: coords.latitude,
       longitude: coords.longitude,
